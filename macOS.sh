95c542afc0a4c428ce3032b8bab2eea2299ecc19
Jacob Hurlburt <jthurlburt818@gmail.com>
2024-09-02 20:16:35 -0500

    Do not need settings for desktop background

--- macOS.sh
+++ macOS.sh
@@ -9,27 +9,3 @@ read
 # Set scroll as traditional instead of natural
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 killall Finder
-
-# Set location for screenshots
-mkdir "${HOME}/Desktop/Screenshots"
-defaults write com.apple.screencapture location "${HOME}/Desktop/Screenshots"
-killall SystemUIServer
-
-# Add Bluetooth to Menu Bar for battery percentages
-defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool true
-killall ControlCenter
-
-# Get the absolute path to the image
-IMAGE_PATH="${HOME}/dotfiles/settings/Desktop.png"
-
-# AppleScript command to set the desktop background
-osascript <<EOF
-tell application "System Events"
-    set desktopCount to count of desktops
-    repeat with desktopNumber from 1 to desktopCount
-        tell desktop desktopNumber
-            set picture to "$IMAGE_PATH"
-        end tell
-    end repeat
-end tell
-EOF
